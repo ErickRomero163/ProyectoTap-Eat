adc36e1e3e1e14432ac20d23a7ce3178cf9a881c
PEDRO-GAR-PAD <zs23004733@estudiantes.uv.mx>
2025-06-12 11:31:30 -0600

    Primeras conexiones al backend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,29 +2,43 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
+import { HttpClient, HttpErrorResponse, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [FormsModule, NgIf]
+  imports: [FormsModule, NgIf, HttpClientModule]  // ✅ importante para HttpClient
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
   mensajeError: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   iniciarSesion() {
-    if (this.email === '10' && this.password === '1') {
-      this.router.navigate(['/vendedor/dashboard-vendedor']);
-    } else if (this.email === '20' && this.password === '2') {
-      this.router.navigate(['/comprador/dashboard-comprador']);
-    } else {
-      this.mensajeError = 'Credenciales inválidas';
-    }
+    this.http.post<any>('http://localhost:8080/api/auth/login', {
+      correo: this.email,
+      contrasena: this.password
+    }).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('rol', response.rol);
+
+        if (response.rol === 'VENDEDOR') {
+          this.router.navigate(['/vendedor/dashboard-vendedor']);
+        } else if (response.rol === 'COMPRADOR') {
+          this.router.navigate(['/comprador/dashboard-comprador']);
+        } else {
+          this.mensajeError = 'Rol no reconocido';
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        this.mensajeError = 'Correo o contraseña incorrectos';
+      }
+    });
   }
 
   crearCuenta() {
